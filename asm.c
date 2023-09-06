f0d931b490f4d1093bfc588fe8fcd4098fa6fe25
wpramio <willianpramio@hotmail.com>
2023-09-06 10:02:54 -0300

    Initial implementation of ASM generation

---
+++ asm.c
@@ -0,0 +1,14 @@
+// author: Willian R. Pramio (00262875)
+// Assembler Code Generation
+
+#include "asm.h"
+
+
+void *generateAsm(TAC* first)
+{
+  FILE *fout;
+  fout = fopen("out.s", "w");
+
+  fprintf(fout, "## FIXED INIT"
+  ""  );
+}
---
+++ asm.h
@@ -0,0 +1,12 @@
+// author: Willian R. Pramio (00262875)
+// Assembler Code Generation
+
+#ifndef ASM_HEADER
+#define ASM_HEADER
+
+#include "tac.h"
+
+
+void *generateAsm(TAC* first);
+
+#endif
